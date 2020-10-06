ed9eab7c3c689df5a50c8b87ade4142d1a3cb8d5
Kyle Carson <elyknosrac@gmail.com>
2020-10-06 16:55:41 -0500

    Update wix.js

--- wix.js
+++ wix.js
@@ -1,26 +1,26 @@
 var styleString = `
 
-#bks-offering-schedule-summary,bks-offering-schedule-summary { display: none; }
+bks-offering-schedule-summary { display: none; }
 
 /* Schedule Online */
 [ng-if*=textCalendarTitle] { visibility: hidden; position: relative; }
 
 [ng-if*=textCalendarTitle]:after {
     visibility: visible; position: absolute; top: 0; left: 0;
-    content: \"Choose a Delivery Date for Your Service\";
+    content: "Choose a Delivery Date for Your Service";
 }
 
 /* Morning/Evening */
-[slots-list-title=\"time-of-day.morning\"], [slots-list-title=\"time-of-day.evening\"] { display: none; }
+[slots-list-title="time-of-day.morning"], [slots-list-title="time-of-day.evening"] { display: none; }
 
 /* Afternoon */
 .time-slots-list__title { display: none }
 
 /* 12:00 */
-[slots-list-title=\"time-of-day.afternoon\"] [role=button] { visibility: hidden; position: relative; }
-[slots-list-title=\"time-of-day.afternoon\"] [role=button]:after {
+[slots-list-title="time-of-day.afternoon"] [role=button] { visibility: hidden; position: relative; }
+[slots-list-title="time-of-day.afternoon"] [role=button]:after {
     visibility: visible; position: absolute; left: 0;
-    content: \"Confirm Date\";
+    content: "Confirm Date";
 }`
 
 const style = document.createElement('style');
